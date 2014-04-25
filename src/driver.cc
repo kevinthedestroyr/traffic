ed451f0872f4749c663cd712e2dfde5d4da96123
kevinthedestroyr <kevinpietsch@gmail.com>
2014-04-24 22:05:35 -0400

    Update driver.cc

--- src/driver.cc
+++ src/driver.cc
@@ -6,15 +6,15 @@ Driver::Driver(Vehicle* const my_vehicle,
                const float& following_factor,
                const float& speed_offset)
     : vehicle_(my_vehicle),
-    following_factor_(following_factor),
-    speed_offset_(speed_offset), road_(NULL) {
+  following_factor_(following_factor),
+  speed_offset_(speed_offset), road_(NULL) {
   
 }
 
 Driver::Driver(Vehicle* const my_vehicle)
     : vehicle_(my_vehicle),
-    following_factor_(kDefaultFollowingFactor),
-    speed_offset_(kDefaultSpeedOffset) {
+  following_factor_(kDefaultFollowingFactor),
+  speed_offset_(kDefaultSpeedOffset) {
 
 }
 
