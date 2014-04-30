e7f750abcd553f361211de52fe4610194cb32fa2
kevinthedestroyr <kevinpietsch@gmail.com>
2014-04-29 21:19:26 -0400

    common defines and constants for use amongst other header and implementation files.

---
+++ include/common.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   common.h
+ * Author: kevin
+ *
+ * Created on April 29, 2014, 8:41 PM
+ */
+
+#ifndef COMMON_H
+#define	COMMON_H
+
+#define DISALLOW_COPY_AND_ASSIGN(TYPE) \
+  TYPE(const TYPE&);                   \
+  TYPE& operator=(const TYPE&)
+
+namespace {
+constexpr float kMPSOverMPH = 0.44704;
+constexpr float kMetersPerMile = 1609.34;
+}
+
+#endif	/* COMMON_H */
+
